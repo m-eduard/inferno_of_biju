68c0a55e89c35837b33e9ccf310fac97643007c4
m-eduard <marineduardconstantin2001@gmail.com>
2021-12-30 01:34:47 +0200

    Debugging stats (problem: in sizeof_int() when n == 0, the returned size was 0, instead of 1 => encountering ) paranthesis instead of the next sign)

--- task-4/checker/checker.c
+++ task-4/checker/checker.c
@@ -10,6 +10,10 @@ int factor(char *p, int *i);
 int sizeof_int(int n) {
     int size = 0;
 
+    if (n == 0) {
+        return 1;
+    }
+
     while (n > 0) {
         n /= 10;
         size += 1;
@@ -49,7 +53,7 @@ int expression(char *p, int *i) {
     //     result = (p[*i - 1] == '+') ? result + term(p, i) : result - term(p, i);
     // }
 
-    for (int k = *i; p[k] != '\0'; ++k) {
+    for (int k = orig_start; p[k] != '\0'; ++k) {
         // if (*i > k) {
         //     k = *i;
         //     score = 0;
@@ -88,47 +92,51 @@ int term(char *p, int *i) {
 
     int score = 0;
 
+    // orig = *i;
+
     // // *i will never exceed strlen(p)
-    // while (p[*i] == '/' || p[*i] == '*') {
-    //     *i += 1;
-    //     result = (p[*i - 1] == '/') ? result / factor(p, i) : result * factor(p, i);
-    // }
-
-    for (int k = orig; p[k] != '\0'; ++k) {
-        // if (*i > k) {
-        //     k = *i;
-        //     score = 0;
-        // }
-
-        if ((p[k] == '*' || p[k] == '/') && score == 0) {
-            // this is the second(ish) term of the addition
-            *i = k + 1;
-
-            int current = factor(p, i);
-            result = (p[k] == '*') ? result * current : result / current;
-        }
-
-        // get a complete paranthese sequence
-        if ((p[k] == '+' || p[k] == '-') && score == 0) {
-            break;
-        }
-
-        if (p[k] == '(') {
-            score += 1;
-        } else if (p[k] == ')') {
-            score -= 1;
-        }
-
-        // this resulted by a call from a subsequence that was inside paranthesis,
-        // so when the last closing paranthesis is reached, the evaluated expression has to end
-        if (score < 0) {
-            // go past the enclosing brace
-            // printf("teapa\n");
-
-            // *i += 1;
-            break;
-        }
+    while (p[*i] == '/' || p[*i] == '*') {
+        *i += 1;
+        result = (p[*i - 1] == '/') ? result / factor(p, i) : result * factor(p, i);
     }
+
+    // // printf("%d %d\n", orig, *i);
+    // for (int k = *i; k < strlen(p); ++k) {
+    //     // if (*i > k) {
+    //     //     k = *i;
+    //     //     score = 0;
+    //     // }
+
+    //     if ((p[k] == '*' || p[k] == '/') && score == 0) {
+    //         // printf("%d\n", k);
+    //         // this is the second(ish) term of the addition
+    //         *i = k + 1;
+
+    //         int current = factor(p, i);
+    //         result = (p[k] == '*') ? result * current : result / current;
+    //     }
+
+    //     // get a complete paranthese sequence
+    //     if ((p[k] == '+' || p[k] == '-') && score == 0) {
+    //         break;
+    //     }
+
+    //     if (p[k] == '(') {
+    //         score += 1;
+    //     } else if (p[k] == ')') {
+    //         score -= 1;
+    //     }
+
+    //     // this resulted by a call from a subsequence that was inside paranthesis,
+    //     // so when the last closing paranthesis is reached, the evaluated expression has to end
+    //     if (score < 0) {
+    //         // go past the enclosing brace
+    //         // printf("teapa\n");
+
+    //         // *i += 1;
+    //         break;
+    //     }
+    // }
 
     // printf("Evaluated term of the sum is: %d\n", result);
     return result;
--- task-4/evaluate2_understood_the_author_logic.c
+++ task-4/evaluate2_understood_the_author_logic.c
@@ -16,6 +16,10 @@
 int sizeof_int(int n) {
     int size = 0;
 
+    if (n == 0) {
+        return 1;
+    }
+
     while (n > 0) {
         n /= 10;
         size += 1;
@@ -53,40 +57,97 @@ int factor(char *p, int *i) {
 }
 
 
-// Evaluates "factor" * "factor" or "factor" / "factor" expressions
+// THIS WORKS
 int term(char *p, int *i) {
     printf("String 2nd: %s\n", p + *i);
+    int orig = *i;
     int result = factor(p, i);
-    // printf("Term is: %d\n", result);
 
     int score = 0;
 
-    while (*i < strlen(p) && (p[*i] == '/' || p[*i] == '*')) {
-        *i += 1;
-        result = (p[*i - 1] == '/') ? result / factor(p, i) : result * factor(p, i);
-    }
-
-    // for (int k = *i; k < strlen(p); ++k) {
-    //     // if I reach a + / -, I have to end this, because the expression that I had
-    //     // to compute ended
-    //     if ((p[k] == '+' || p[k] == '-') && score == 0) {
-    //         break;
-    //     }
-
-    //     if ((p[k] == '/' || p[k] == '*') && score == 0) {
-    //         *i = k + 1; 
-    //         result = (p[k] == '/') ? result / factor(p, i) : result * factor(p, i);
-    //     }
-
-    //     if (p[k] == '(') {
-    //         result += 1;
-    //     } else if (p[k] == ')') {
-    //         result -= 1;
-    //     }
+    // // *i will never exceed strlen(p)
+    // while (p[*i] == '/' || p[*i] == '*') {
+    //     *i += 1;
+    //     result = (p[*i - 1] == '/') ? result / factor(p, i) : result * factor(p, i);
     // }
 
+    printf("the bad: %d\n", *i);
+    for (int k = *i; p[k] != '\0'; ++k) {
+        // if (*i > k) {
+        //     k = *i;
+        //     score = 0;
+        // }
+
+        if ((p[k] == '*' || p[k] == '/') && score == 0) {
+            printf("%d\n", k);
+            // this is the second(ish) term of the addition
+            *i = k + 1;
+
+            int current = factor(p, i);
+            result = (p[k] == '*') ? result * current : result / current;
+        }
+
+        // get a complete paranthese sequence
+        if ((p[k] == '+' || p[k] == '-') && score == 0) {
+            break;
+        }
+
+        if (p[k] == '(') {
+            score += 1;
+        } else if (p[k] == ')') {
+            score -= 1;
+        }
+
+        // this resulted by a call from a subsequence that was inside paranthesis,
+        // so when the last closing paranthesis is reached, the evaluated expression has to end
+        if (score < 0) {
+            // go past the enclosing brace
+            // printf("teapa\n");
+
+            // *i += 1;
+            break;
+        }
+    }
+
+    // printf("Evaluated term of the sum is: %d\n", result);
     return result;
 }
+
+
+// // Evaluates "factor" * "factor" or "factor" / "factor" expressions
+// int term(char *p, int *i) {
+//     printf("String 2nd: %s\n", p + *i);
+//     int result = factor(p, i);
+//     // printf("Term is: %d\n", result);
+
+//     int score = 0;
+
+//     while (*i < strlen(p) && (p[*i] == '/' || p[*i] == '*')) {
+//         *i += 1;
+//         result = (p[*i - 1] == '/') ? result / factor(p, i) : result * factor(p, i);
+//     }
+
+//     // for (int k = *i; k < strlen(p); ++k) {
+//     //     // if I reach a + / -, I have to end this, because the expression that I had
+//     //     // to compute ended
+//     //     if ((p[k] == '+' || p[k] == '-') && score == 0) {
+//     //         break;
+//     //     }
+
+//     //     if ((p[k] == '/' || p[k] == '*') && score == 0) {
+//     //         *i = k + 1; 
+//     //         result = (p[k] == '/') ? result / factor(p, i) : result * factor(p, i);
+//     //     }
+
+//     //     if (p[k] == '(') {
+//     //         result += 1;
+//     //     } else if (p[k] == ')') {
+//     //         result -= 1;
+//     //     }
+//     // }
+
+//     return result;
+// }
 
 // Evaluates "term" + "term" or "term" - "term" expressions
 int expression(char *p, int *i) {
@@ -133,11 +194,18 @@ int expression(char *p, int *i) {
 
 int main(void) {
     // char *string = strdup("(((1+2)*10+200)*2)+(2+1)+2");
-    char *string = strdup("(1+2)*(3*4/10+1)+(12+2*(3/10))");
+    // char *string = strdup("(1+2)*(3*4/10+1)+(12+2*(3/10))");
+
+    // this is the string that generates a jump over the last division,
+    // when the evaluation of term starts from the updated pointer
+    char *string = strdup("(36*(90-0))/(89)");
+
+// (7+4+4+(1*(13+7)+3)*(66-9)*36*(90-0))/(89)
 
     int current_pos = 0;
     printf("The result: %d\n", expression(string, &current_pos));
-    printf("%d\n", (1+2)*(3*4/10+1)+(12+2*(3/10)));
+    // printf("%d\n", (1+2)*(3*4/10+1)+(12+2*(3/10)));
+    printf("%d\n", (36*(90-0))/(89));
 
     return 0;
 }
